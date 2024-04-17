598ee6072281ff3cbab90d64a8ae7a13a07644a3
Your Name <you@example.com>
2024-04-17 19:28:26 +0530

    first code added

---
+++ s3_bucket.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "raghvendra127345" {
+  bucket = "raghvendra127345"  # Specify your desired bucket name
+  acl    = "private"  # Set the access control list for the bucket (private, public-read, public-read-write, aws-exec-read, authenticated-read)
+
+  tags = {
+    Environment = "test"
+  }
+}
