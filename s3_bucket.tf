6870fcb3cb284b0fec49eaf8f3472baa8d5ee392
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-10 22:12:18 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "raghvendra127345" {
   bucket = "raghvendra127345"  # Specify your desired bucket name
-  acl    = "private"  # Set the access control list for the bucket (private, public-read, public-read-write, aws-exec-read, authenticated-read)
+  #acl    = "private"  # Set the access control list for the bucket (private, public-read, public-read-write, aws-exec-read, authenticated-read)
 
   tags = {
     Environment = "test"
