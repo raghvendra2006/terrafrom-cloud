433fcfbfebde4816721f796c784695eb4531ef63
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-10 21:38:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-south-1"  # Specify the region where you want to create the bucket
-}
-
-cloud {
-	organization = "test-learning2006"
-	workspace {
-		name = "devops-aws-myapp-dev"
-	}
+  region = "ap-south-1"
+  assume_role {
+    role_arn = var.TFC_AWS_RUN_ROLE_ARN
+  }
 }
