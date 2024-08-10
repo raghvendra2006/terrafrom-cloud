c6a24beb2bfd3d84944c5b162ec9467faa6ccde6
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-10 22:07:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
   assume_role {
-    role_arn = "arn:aws:iam::590183912396:role/terraform-openid"
+    role_arn = "arn:aws:iam::590183912396:role/tfc-role"
   }
 }
