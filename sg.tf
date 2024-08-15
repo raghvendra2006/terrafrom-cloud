112050df35aa56130102846552535cdaab0fdde7
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-15 12:48:22 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow inbound traffic on port 443"
+  vpc_id      = "vpc-0bd5f385494b4cbec"  # Replace with your VPC ID
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["183.87.190.57/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
