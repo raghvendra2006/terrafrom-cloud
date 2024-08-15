620a8360285c492ff41343032c3ee9945550b2e0
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-15 13:01:23 +0530

    Update ket_pair.tf

--- ket_pair.tf
+++ ket_pair.tf
@@ -7,14 +7,3 @@ resource "aws_key_pair" "ec2_key" {
   key_name   = "${var.key_name_prefix}-${terraform.workspace}"
   public_key = tls_private_key.ec2_key.public_key_openssh
 }
-
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.ec2_key.private_key_pem
-  filename = "${path.module}/ec2-key.pem"
-  sensitive = true
-
-  # Setting permissions for the private key
-  provisioner "local-exec" {
-    command = "chmod 400 ${path.module}/ec2-key.pem"
-  }
-}
