e0f444005ab3e93b3c71f53259ec5ef25a846cdc
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-15 12:55:20 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["183.87.190.57/32"]
   }
 
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["183.87.190.57/32"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
