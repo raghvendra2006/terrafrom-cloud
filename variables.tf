5d31679de3610b351703e05ebea22db13f0617d1
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-15 13:11:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.micro"
 }
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  default     = data.aws_ami.latest_amazon_linux_2.id
-}
