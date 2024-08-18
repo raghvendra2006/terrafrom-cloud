d7b2e249d38613ade1b3bc6a3e87a62b6e8e26e1
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-18 11:56:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,6 @@ resource "aws_instance" "web_server" {
   instance_type = var.instance_type
   key_name      = aws_key_pair.ec2_key.key_name
   security_groups = [aws_security_group.web_sg.name]
-  associate_public_ip_address = true
-  instance_state = "running"
   root_block_device {
   iops = 3000
   throughput = 125
