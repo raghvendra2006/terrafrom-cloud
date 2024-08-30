713233850b6fa29600d38d10c64ae15a166238b1
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-30 22:36:24 +0530

    Update main.tf

--- sg-test-proj/main.tf
+++ sg-test-proj/main.tf
@@ -1,13 +1,13 @@
-#module "vpc1" {
-#  source    = "./modules/vpc"
-#  cidr_block = "10.10.10.0/24"
-#  vpc_name   = "my-main-vpc2"
-#  create_security_group = true
-#}
-#
-#module "vpc2" {
-#  source    = "./modules/vpc"
-#  cidr_block = "10.10.11.0/24"
-#  vpc_name   = "my-main-vpc2"
-#  create_security_group = true
-#}
+module "vpc1" {
+  source    = "./modules/vpc"
+  cidr_block = "10.10.10.0/24"
+  vpc_name   = "my-main-vpc2"
+  create_security_group = no
+}
+
+module "vpc2" {
+  source    = "./modules/vpc"
+  cidr_block = "10.10.11.0/24"
+  vpc_name   = "my-main-vpc2"
+  create_security_group = no
+}
