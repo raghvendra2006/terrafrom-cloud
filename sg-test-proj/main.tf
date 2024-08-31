3fd8594ea2a6b1afc3f16be483908023f2745fa7
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2024-08-31 19:42:03 +0530

    Update main.tf

--- sg-test-proj/main.tf
+++ sg-test-proj/main.tf
@@ -5,9 +5,9 @@ module "vpc1" {
   create_security_group = true
 }
 
-#module "vpc2" {
-#  source    = "./modules/vpc"
-#  cidr_block = "10.10.11.0/24"
-#  vpc_name   = "my-main-vpc2"
-#  create_security_group = true
-#}
+module "vpc2" {
+  source    = "./modules/vpc"
+  cidr_block = "10.10.11.0/24"
+  vpc_name   = "my-main-vpc2"
+  create_security_group = true
+}
