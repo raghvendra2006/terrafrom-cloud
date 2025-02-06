84d0522622381db68ed59ce1841202ab8cbb951b
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-02-06 20:43:33 +0530

    Create bucket.tf

---
+++ test-tf-role/bucket.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-unique-bucket-name-ap-south-1"  # Change to a unique bucket name
+
+  tags = {
+    Name        = "ExampleBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
