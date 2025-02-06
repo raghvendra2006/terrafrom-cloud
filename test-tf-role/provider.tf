39b5c4c406c494c785c6d281eecc5def938e5922
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-02-06 20:50:38 +0530

    Update provider.tf

--- test-tf-role/provider.tf
+++ test-tf-role/provider.tf
@@ -11,6 +11,6 @@ provider "aws" {
   region = "ap-south-1"
   
   assume_role {
-    role_arn = "arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"  # Replace with your IAM role ARN
+    role_arn = "arn:aws:iam::590183912396:role/testrole1"  # Replace with your IAM role ARN
   }
 }
