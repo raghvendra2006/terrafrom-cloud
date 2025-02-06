a8125438d3fb59bc9fa2fae5e02f9e2ce056a08f
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-02-06 21:46:26 +0530

    Create eks.tf

---
+++ test-tf-role/eks.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-0417f0c5554f6dd3b"]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+resource "aws_iam_role" "eks_fargate_role" {
+  name = "eks-fargate-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_fargate_policy" {
+  role       = aws_iam_role.eks_fargate_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
+resource "aws_eks_fargate_profile" "fargate" {
+  cluster_name           = aws_eks_cluster.eks.name
+  fargate_profile_name   = "fargate-profile"
+  pod_execution_role_arn = aws_iam_role.eks_fargate_role.arn
+  subnet_ids             = ["subnet-0417f0c5554f6dd3b"]
+
+  selector {
+    namespace = "default"
+  }
+}
