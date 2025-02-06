b052fb4dd710da38f080c16409959c730aaf579c
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-02-06 21:49:59 +0530

    Update eks.tf

--- test-tf-role/eks.tf
+++ test-tf-role/eks.tf
@@ -21,6 +21,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "eks" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.29"  # Updated to EKS version 1.29
 
   vpc_config {
     subnet_ids = ["subnet-0417f0c5554f6dd3b"]
