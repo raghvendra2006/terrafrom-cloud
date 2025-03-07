d30264b910ee8ca0e7d9fbbc3b837b6535fd112b
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-03-07 13:13:21 +0530

    Update vpc.tf

--- test-tf-role/vpc.tf
+++ test-tf-role/vpc.tf
@@ -1,8 +1,17 @@
 resource "aws_vpc" "vpc1" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.10.10.0/24"
 
   tags = {
     Name            = "${var.environment}-vpc"
     "VPC-Environment" = var.environment
+  }
+}
+
+resource "aws_vpc" "vpc2" {
+  cidr_block = "10.10.11.0/24"
+
+  tags = {
+    Name            = "${var.environment}-vpc"
+    "VPC-Environment" = var.environment1
   }
 }
