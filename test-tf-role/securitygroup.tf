a64761c8a70c240e19579cd5023059dbf3680feb
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-03-07 20:24:31 +0530

    Update securitygroup.tf

--- test-tf-role/securitygroup.tf
+++ test-tf-role/securitygroup.tf
@@ -8,9 +8,11 @@ resource "aws_security_group" "sg1" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.region == "ap-south-1" ? 
-      (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
-      (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    ccidr_blocks = (
+      var.region == "ap-south-1" ? 
+        (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
+        (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    )
   }
 
   # Rule for UDP 3306-3307
@@ -18,10 +20,11 @@ resource "aws_security_group" "sg1" {
     from_port   = 3306
     to_port     = 3307
     protocol    = "udp"
-    cidr_blocks = var.region == "ap-south-1" ? 
-      (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
-      (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
-  }
+    cidr_blocks = (
+      var.region == "ap-south-1" ? 
+        (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
+        (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    )
 }
 
 resource "aws_security_group" "sg2" {
@@ -34,9 +37,11 @@ resource "aws_security_group" "sg2" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.region == "ap-south-1" ? 
-      (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
-      (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    cidr_blocks = (
+      var.region == "ap-south-1" ? 
+        (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
+        (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    )
   }
 
   # Rule for UDP 3306-3307
@@ -44,8 +49,10 @@ resource "aws_security_group" "sg2" {
     from_port   = 3306
     to_port     = 3307
     protocol    = "udp"
-    cidr_blocks = var.region == "ap-south-1" ? 
-      (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
-      (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    cidr_blocks = (
+      var.region == "ap-south-1" ? 
+        (var.environment == "prod" ? ["10.0.1.0/32", "10.0.1.1/32"] : ["10.0.2.0/32", "10.0.2.1/32"]) :
+        (var.environment == "prod" ? ["172.16.1.0/32", "172.16.1.1/32"] : ["172.16.2.0/32", "172.16.2.1/32"])
+    )
   }
 }
