3aeee5b0ff77324c409c6149b191c8dc68a60d6b
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-03-07 12:51:44 +0530

    Create variable.tf

---
+++ test-tf-role/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region where the VPC will be created"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment for the VPC (prod/uat/dev)"
+  type        = string
+  validation {
+    condition     = contains(["prod", "uat", "dev"], var.environment)
+    error_message = "Allowed values for environment are 'prod', 'uat', or 'dev'."
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
