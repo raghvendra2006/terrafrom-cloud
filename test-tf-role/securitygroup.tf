ca1baac66ce6a9a52f368a7a4f31aa347849926e
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-03-07 12:59:28 +0530

    Create securitygroup.tf

---
+++ test-tf-role/securitygroup.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "main" {
+  name        = "${var.environment}-${var.region}-sg"
+  description = "Security group for ${var.environment} in ${var.region}"
+  vpc_id      = module.vpc.vpc_id
+
+ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.environment == "prod" ? (
+      var.region == "aps1" ? ["10.1.1.1/32", "10.1.2.1/32"] : 
+      var.region == "aps2" ? ["11.1.1.1/32", "11.1.2.1/32"] : []
+    ) : (
+      var.region == "aps1" ? ["10.2.1.1/32", "10.2.2.1/32"] : 
+      var.region == "aps2" ? ["11.2.1.1/32", "11.2.2.1/32"] : []
+    )
+  }
+
+  }
