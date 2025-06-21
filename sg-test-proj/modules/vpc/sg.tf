dc7200d39224d58e01e80a28afd1d7c769f87860
raghvendra2006 <110380456+raghvendra2006@users.noreply.github.com>
2025-06-21 18:41:29 +0530

    Update sg.tf

--- sg-test-proj/modules/vpc/sg.tf
+++ sg-test-proj/modules/vpc/sg.tf
@@ -20,6 +20,22 @@ resource "aws_security_group" "example_sg" {
     cidr_blocks = ["1.1.1.1/32","4.4.4.4/32"]
   }
 
+  ingress {
+    description = "Allow SSH from 2.2.2.2/32"
+    from_port   = 53
+    to_port     = 63
+    protocol    = "tcp"
+    cidr_blocks = ["2.2.2.2/32","3.3.3.3/32"]
+  }
+
+  ingress {
+    description = "Allow port range 23-35 from 1.1.1.1/32"
+    from_port   = 1023
+    to_port     = 2035
+    protocol    = "tcp"
+    cidr_blocks = ["1.1.1.1/32","4.4.4.4/32"]
+  }
+
   tags = {
     Name = var.sg_name
   }
